e8afae9da3213e8063f1a8074814c36b82608185
Cerome360 <61514307+Cerome360@users.noreply.github.com>
2025-06-23 10:59:25 +0100

    Add files via upload

---
+++ Python Modules/modules.py
@@ -0,0 +1,12 @@
+from math import pi
+import sys
+import random as rdm
+from enum import Enum
+from rps import rock_paper_scissors
+
+print(pi)
+
+# for item in dir(rdm):
+#   print(item)
+
+rock_paper_scissors
